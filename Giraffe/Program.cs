cb0a88750f5ee982dd8ae78fee3a5d4b9d884731
Tijil <tijil2008@gmail.com>
2021-08-04 08:15:18 +0530

    intial commit

---
+++ Giraffe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
